0c6bba83e3212035b4819a468633b87c753a63f8
Magno <workmag@outlook.com>
2017-08-18 11:24:59 -0300

    Primeira Versão do Projeto

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<?php
+
+ ?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Delícia Gelada</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/master.css">
+    <script type="text/javascript"
+    src="javascript/jquery-3.2.1.min.js">
+    </script>
+  </head>
+  <body>
+    <div id="menu_superior">
+      <nav>
+        <div id="logo">
+          logo
+        </div>
+        <div id="menu_links">
+          <!-- <div class="opcoes">
+            <a href="#">Home</a>
+          </div>
+          <div class="opcoes">
+            <a href="#">Home</a>
+          </div>
+          <div class="opcoes">
+            <a href="#">Home</a>
+          </div>
+          <div class="opcoes">
+            <a href="#">Home</a>
+          </div> -->
+          <ul>
+            <li class="opcoes">
+              <a href="#">Home</a>
+            </li>
+            <li class="opcoes">
+              <a href="#">A moda do verão</a>
+            </li>
+            <li class="opcoes">
+              <a href="#">A importância do suco natural</a>
+            </li>
+            <li class="opcoes">
+              <a href="#">Promoções</a>
+            </li>
+            <li class="opcoes">
+              <a href="#">Nossos Ambientes</a>
+            </li>
+            <li class="opcoes">
+              <a href="#">Suco do mês</a>
+            </li>
+          </ul>
+        </div>
+        <form class="" action="index.html" method="post">
+          <div class="autenticacao">
+            Usuário:
+            <div class="">
+              <input type="text" placeholder="Insira seu nome de usuario" name="txtUsuario">
+            </div>
+            Senha:
+            <div class="">
+              <input type="password" placeholder="Insira sua senha" name="txt_senha" value="">
+            </div>
+            <input type="submit" name="btnLogar" value="ok">
+          </div>
+        </form>
+      </nav>
+    </div>
+    <header>
+      <div id="redes_sociais">
+        <div class="">
+          facebook
+        </div>
+        <div class="">
+          instagram
+        </div>
+        <div class="">
+          linkedIn
+        </div>
+      </div>
+    </header>
+    <section>
+      <div id="menu_lateral">
+        <nav>
+
+        </nav>
+      </div>
+      <div id="conteudo">
+        <?php
+           ?>
+           <div class="produto">
+             <ul>
+               <li>Nome: <?php  ?></li>
+               <li>Descrição: <?php  ?></li>
+               <li>Preço: <?php  ?></li>
+             </ul>
+           </div>
+           <div class="produto">
+             <ul>
+               <li>Nome: <?php  ?></li>
+               <li>Descrição: <?php  ?></li>
+               <li>Preço: <?php  ?></li>
+             </ul>
+           </div>
+           <div class="produto">
+             <ul>
+               <li>Nome: <?php  ?></li>
+               <li>Descrição: <?php  ?></li>
+               <li>Preço: <?php  ?></li>
+             </ul>
+           </div>
+        <?php
+           ?>
+      </div>
+    </section>
+    <footer>
+
+    </footer>
+  </body>
+</html>
