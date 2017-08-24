445d75250016c0857ab0900b0a5fe0c930b843ab
Magno <workmag@outlook.com>
2017-08-24 11:27:45 -0300

    Menu recriado (funcionando, mas precisa arrumar o design)

--- index.php
+++ index.php
@@ -13,104 +13,104 @@
     </script>
   </head>
   <body>
-    <div id="menu_superior">
-      <nav>
-        <div id="logo">
-          logo
-        </div>
-        <div id="menu_links">
-          <!-- <div class="opcoes">
-            <a href="#">Home</a>
+    <!-- MENU SUPERIOR -->
+    <div id="campo_menu_superior">
+
+      <div id="menu_superior">
+
+        <div id="campo_items_menu">
+          <div id="logo">
+            <h1>Logo</h1>
           </div>
-          <div class="opcoes">
-            <a href="#">Home</a>
-          </div>
-          <div class="opcoes">
-            <a href="#">Home</a>
-          </div>
-          <div class="opcoes">
-            <a href="#">Home</a>
-          </div> -->
-          <ul>
-            <li class="opcoes">
-              <a href="#">Home</a>
-            </li>
-            <li class="opcoes">
-              <a href="#">A moda do verão</a>
-            </li>
-            <li class="opcoes">
-              <a href="#">A importância do suco natural</a>
-            </li>
-            <li class="opcoes">
-              <a href="#">Promoções</a>
-            </li>
-            <li class="opcoes">
-              <a href="#">Nossos Ambientes</a>
-            </li>
-            <li class="opcoes">
-              <a href="#">Suco do mês</a>
-            </li>
-          </ul>
-        </div>
-        <form class="" action="index.html" method="post">
-          <div class="autenticacao">
-            Usuário:
-            <div class="">
-              <input type="text" placeholder="Insira seu nome de usuario" name="txtUsuario">
+          <nav>
+            <div id="caixa_menu_superior">
+              <ul>
+                <li class="opcoes">
+                  <a href="#">Home</a>
+                </li>
+                <li class="opcoes">
+                  <a href="#">A moda do verão</a>
+                </li>
+                <li class="opcoes">
+                  <a href="#">A importância do suco natural</a>
+                </li>
+                <li class="opcoes">
+                  <a href="#">Promoções</a>
+                </li>
+                <li class="opcoes">
+                  <a href="#">Nossos Ambientes</a>
+                </li>
+                <li class="opcoes">
+                  <a href="#">Suco do mês</a>
+                </li>
+              </ul>
             </div>
-            Senha:
-            <div class="">
-              <input type="password" placeholder="Insira sua senha" name="txt_senha" value="">
+          </nav>
+          <form class="frmLogin" action="index.php" method="post">
+            <div class="login">
+              <div class="">
+                Login:
+              </div>
+              <input type="text" name="txtLogin" value="" placeholder="       Insira seu login" required>
             </div>
-            <input type="submit" name="btnLogar" value="ok">
-          </div>
-        </form>
-      </nav>
+            <div class="login">
+              <div class="">
+                Senha:
+              </div>
+              <input type="password" name="txtSenha" value="" placeholder="       Insira sua senha" required>
+            </div>
+            <div class="login">
+              <input type="submit" name="btnEntrar" value="Entrar">
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+    <!-- SLIDER -->
     <header>
       <div id="redes_sociais">
-        <div class="">
-          facebook
+        <div class="redes">
+          <img src="" alt="">
+          <a href="#">facebook</a>
         </div>
-        <div class="">
-          instagram
+        <div class="redes">
+          <img src="" alt="">
+          <a href="#">instagram</a>
         </div>
-        <div class="">
-          linkedIn
+        <div class="redes">
+          <img src="" alt="">
+          <a href="#">twitter</a>
         </div>
       </div>
     </header>
+    <!-- CONTEUDO -->
     <section>
+      <!-- MENU LATERAL -->
       <div id="menu_lateral">
         <nav>
-
+          <h2>menulateral</h2>
         </nav>
       </div>
+      <!-- PRODUTOS -->
       <div id="conteudo">
         <?php
+          //$i = 0;
+          //while ($i < 10) {
+            # code...
+          //  $i = $i + 1;
            ?>
-           <div class="produto">
-             <ul>
-               <li>Nome: <?php  ?></li>
-               <li>Descrição: <?php  ?></li>
-               <li>Preço: <?php  ?></li>
-             </ul>
-           </div>
-           <div class="produto">
-             <ul>
-               <li>Nome: <?php  ?></li>
-               <li>Descrição: <?php  ?></li>
-               <li>Preço: <?php  ?></li>
-             </ul>
-           </div>
-           <div class="produto">
-             <ul>
-               <li>Nome: <?php  ?></li>
-               <li>Descrição: <?php  ?></li>
-               <li>Preço: <?php  ?></li>
-             </ul>
+           <div id="faixa_produto">
+             <div class="produto">
+               <ul>
+                 <li>Nome: <?php  ?></li>
+                 <li>Descrição: <?php  ?></li>
+                 <li>Preço: <?php  ?></li>
+               </ul>
+             </div>
+
            </div>
         <?php
+        //}
            ?>
       </div>
     </section>
